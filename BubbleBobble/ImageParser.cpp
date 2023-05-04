65d4251be7c12e148ebdabf5469324066891d540
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-05-04 15:30:13 +0200

    fix: correct amount of byte jumps

--- BubbleBobble/ImageParser.cpp
+++ BubbleBobble/ImageParser.cpp
@@ -5,23 +5,33 @@
 
 #include <iostream>
 
+namespace aze
+{
+    struct Uint24 final
+    {
+        uint8_t r{};
+        uint8_t g{};
+        uint8_t b{};
+    };
+}
+
 aze::ImageParser::ImageParser(const std::string& inputFile)
-    :m_InputFile{inputFile}
+    :m_InputFile{ inputFile }
 {
     // Load image from file
     m_Image = ResourceManager::GetInstance().LoadSurface(inputFile);
-   
+
     auto sdlImage = m_Image->GetSDLSurface();
     // Extract colors from all pixels in image
     SDL_LockSurface(sdlImage);
-    Uint32* pixels = (Uint32*)sdlImage->pixels;
+    Uint24* pixels = (Uint24*)sdlImage->pixels;
     Uint8 r, g, b;
     for (int y{ 0 }; y < sdlImage->h; ++y)
     {
         for (int x{ 0 }; x < sdlImage->w; ++x)
         {
-            Uint32 pixel = pixels[x + (y * sdlImage->w)];
-            SDL_GetRGB(pixel, sdlImage->format, &r, &g, &b);
+            Uint24 pixel = pixels[x + (y * sdlImage->w)];
+            SDL_GetRGB(*reinterpret_cast<Uint32*>(&pixel), sdlImage->format, &r, &g, &b);
             PointCol pc{};
             pc.col = { r, g, b, 255 };
             pc.point = { x, y };
@@ -29,17 +39,6 @@ aze::ImageParser::ImageParser(const std::string& inputFile)
         }
     }
     SDL_UnlockSurface(sdlImage);
-
-    /*std::cout << "Done parsing image:\n";
-    int p{0};
-    for (auto& pixel : m_Pixels)
-    {
-        std::cout << "[" << p << "] P: " 
-            << pixel.point.x << " , " << pixel.point.y 
-            << " Col: " << pixel.col.r << " , " << pixel.col.g << " , " << pixel.col.b << "\n";
-        ++p;
-    }
-    std::cout << "\n\n\n";*/
 }
 
 const std::vector<aze::ImageParser::PointCol>& aze::ImageParser::GetPixels()
