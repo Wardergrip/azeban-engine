2f6e15a199ecb3fd96bd67f602e7fd2d67dc527b
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-06-10 13:57:18 +0200

    feat: collision & static colliders

--- Minigin/BoxColliderComponent.h
+++ Minigin/BoxColliderComponent.h
@@ -13,6 +13,7 @@ namespace aze
 		BoxColliderComponent(GameObject* pParent)
 			:Component{pParent}
 			,m_Rect{ 24.f,24.f }
+			,m_IsStatic{false}
 		{
 			CollisionManager::GetInstance().AddCollider(this);
 		}
@@ -24,11 +25,15 @@ namespace aze
 		virtual void Update() override
 		{
 			m_Rect.topLeft = GetGameObject()->GetTransform().GetWorldPosition();
+			std::cout << this << " " << m_Rect.topLeft.x << " , " << m_Rect.topLeft.y << "\n";
 		}
 
 		const Rect& GetHitbox() const { return m_Rect; }
+		void SetStatic(bool state) { m_IsStatic = state; }
+		bool IsStatic() const { return m_IsStatic; }
 
 	private:
 		Rect m_Rect;
+		bool m_IsStatic;
 	};
 }
--- Minigin/CollisionManager.cpp
+++ Minigin/CollisionManager.cpp
@@ -17,17 +17,79 @@ void aze::CollisionManager::FixedUpdate()
 {
 	for (auto& pBoxCollider : m_BoxColliders)
 	{
-		for (auto& pOtherBoxCollider : m_BoxColliders)
+		if (pBoxCollider->IsStatic()) continue;
+		std::vector<BoxColliderComponent*> colliding{GetColliding(pBoxCollider)};
+		const auto& hitBox{ pBoxCollider->GetHitbox() };
+		float totalOffsetX{ 0 };
+		float totalOffsetY{ 0 };
+		for (const auto& collider : colliding)
 		{
-			if (pBoxCollider == pOtherBoxCollider) continue;
-			if (pBoxCollider->GetHitbox().IsOverlapping(pOtherBoxCollider->GetHitbox()))
+			// Source: https://github.com/BramVernimmen/Prog4_Engine/blob/main/Minigin/CollisionManager.cpp
+			const auto& otherHitBox{ collider->GetHitbox() };
+			const bool hitIsLeft{ hitBox.topLeft.x <= otherHitBox.topLeft.x };
+			const bool hitIsAbove{ hitBox.topLeft.y <= otherHitBox.topLeft.y };
+
+			float offsetX{ 0 };
+			float offsetY{ 0 };
+
+			if (hitIsLeft)
 			{
-				std::cout << "Overlapping!\n";
+				offsetX = otherHitBox.topLeft.x - (hitBox.topLeft.x + hitBox.width);
 			}
 			else
 			{
-				std::cout << "Not overlapping\n";
+				offsetX = (hitBox.topLeft.x - (otherHitBox.topLeft.x + otherHitBox.width)) * -1;
 			}
+
+			if (hitIsAbove)
+			{
+				offsetY = otherHitBox.topLeft.y - (hitBox.topLeft.y + hitBox.height);
+			}
+			else
+			{
+				offsetY = (hitBox.topLeft.y - (otherHitBox.topLeft.y + otherHitBox.height)) * -1;
+			}
+
+			// Take the lowest offset, this is the shortest path to no longer overlap
+			if (std::abs(offsetX) < std::abs(offsetY))
+			{
+				offsetY = 0;
+			}
+			else
+			{
+				offsetX = 0;
+			}
+
+			totalOffsetX += offsetX;
+			totalOffsetY += offsetY;
+		}
+
+		// Make sure we won't overcorrect too much
+		{
+			const float hitsAmountRatio{ 1.f / std::max(colliding.size(),static_cast<size_t>(1)) };
+			totalOffsetX *= hitsAmountRatio;
+			totalOffsetY *= hitsAmountRatio;
+		}
+		const glm::vec3& localPos{ pBoxCollider->GetGameObject()->GetTransform().GetLocalPosition()};
+		pBoxCollider->GetGameObject()->GetTransform().SetPosition(localPos.x + totalOffsetX, localPos.y + totalOffsetY);
+	}
+}
+
+std::vector<aze::BoxColliderComponent*> aze::CollisionManager::GetColliding(BoxColliderComponent* pBoxCollider) const
+{
+	std::vector<BoxColliderComponent*> colliding;
+	if (pBoxCollider == nullptr) return std::move(colliding);
+
+	const auto& hitbox = pBoxCollider->GetHitbox();
+	for (auto& pOtherBoxCollider : m_BoxColliders)
+	{
+		if (pBoxCollider == pOtherBoxCollider) continue;
+
+		if (hitbox.IsOverlapping(pOtherBoxCollider->GetHitbox()))
+		{
+			colliding.emplace_back(pOtherBoxCollider);
 		}
 	}
+
+	return std::move(colliding);
 }
